f4c7d5a54bf7b8c15d8efeb14d9aba35c1783a94
THO-COR54_hdfclife <THO_COR54@HDFCLIFE.COM>
2026-07-30 00:17:16 +0530

    [Laxman] - Added data provider to make it data driven

--- src/test/java/com/automation/dataproviders/LoginDataProvider.java
+++ src/test/java/com/automation/dataproviders/LoginDataProvider.java
@@ -3,6 +3,7 @@ package com.automation.dataproviders;
 import com.automation.pojos.TestData;
 import com.automation.pojos.User;
 import com.google.gson.Gson;
+import org.testng.annotations.DataProvider;
 
 import java.io.File;
 import java.io.FileReader;
@@ -10,24 +11,30 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class LoginDataProvider {
-    @da
-    public Iterator<Object[]> LoginDataProvider() {
+    @DataProvider(name = "LoginDataProvider")
+    public Iterator<Object[]> loginDataProvider() {
         Gson gson = new Gson();
         File testDataFile = new File(System.getProperty("user.dir") + "/testData/loginTestData.json");
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(testDataFile);
+
+        try (FileReader fileReader = new FileReader(testDataFile)) {
+
             TestData testData = gson.fromJson(fileReader, TestData.class);
-            List<Object[]> dataToReturn = new ArrayList<Object[]>();
+
+            if (testData == null || testData.getUsers() == null) {
+                throw new RuntimeException("User list is NULL → JSON does not match TestData POJO");
+            }
+
+            List<Object[]> dataToReturn = new ArrayList<>();
             for (User user : testData.getUsers()) {
                 dataToReturn.add(new Object[]{user});
             }
 
             return dataToReturn.iterator();
-
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error loading JSON Test Data → " + e);
         }
     }
+
 }
